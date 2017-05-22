896209aa6aa864b17a0d4b79ebfcc443700eb354
johnlenon <johnlenonmaghanoy@gmail.com>
2017-05-22 14:11:16 +0800

    Updated test.

--- stagn-test.js
+++ stagn-test.js
@@ -1,5 +1,4 @@
 
 const stagn = require( "./stagn.js" );
 
-console.log( require( "util" ).inspect( stagn( function Hello( ){ },
-{ "yeah": "world" }, function why( ){ } ), { "showHidden": true } ) );
+console.log( require( "util" ).inspect( stagn( function Hello( ){ }, { "yeah": "world" }, function why( ){ } ), { "showHidden": true } ) );
