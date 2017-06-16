7628b51c4b902d470b1e3baa4ca01d467c918e87
johnlenon <johnlenonmaghanoy@gmail.com>
2017-06-16 15:08:22 +0800

    Updated server/client test.

--- stagn-test.js
+++ stagn-test.js
@@ -1,4 +1,15 @@
 
+const assert = require( "assert" );
 const stagn = require( "./stagn.js" );
 
-console.log( require( "util" ).inspect( stagn( function Hello( ){ }, { "yeah": "world" }, function why( ){ } ), { "showHidden": true } ) );
+assert.equal( typeof stagn( function Hello( ){ }, { "yeah": "world" }, function why( ){ } ) == "function",
+				true, "should return true" );
+
+let Test = stagn( function Hello( ){ }, { "yeah": "world" }, function why( ){ } );
+console.log( require( "util" ).inspect( Test, { "showHidden": true } ) );
+
+assert.equal( Test.yeah, "world", "should return 'world'" );
+
+assert.equal( typeof Test.why == "function", true, "should return true" );
+
+console.log( "ok" );
